d33544b48312a5611370de6606097d1060a98120
Ryan E Davis <redavi19@asu.edu>
2026-09-07 12:58:03 -0400

    Fix ICA Master Turnstile site key typo

--- src/master/MasterPortal.jsx
+++ src/master/MasterPortal.jsx
@@ -6,7 +6,7 @@ const API_URL =
 
 const TURNSTILE_SITE_KEY =
   import.meta.env.VITE_ICA_MASTER_TURNSTILE_SITE_KEY ||
-  '0x4AAAAAAErtQB79-x1-UTHQ'
+  '0x4AAAAAAErtQB79-xi-UTHQ'
 
 const PRODUCT_LINKS = {
   scenepilot: 'https://scenepilot.ryanedavis.workers.dev/',
